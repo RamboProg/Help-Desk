ecb4319d6259e493c95372efb2a4190bbf416bda
RamboProg <smido964@gmail.com>
2023-11-21 16:35:22 +0200

    Added middleware folder and some important files

---
+++ controllers/userController.js
@@ -0,0 +1,3 @@
+/* Use for handling specific routes or groups of routes. 
+*Each controller file might correspond to a specific resource or functionality in your application. 
+For example, you could have a userController.js file that handles user-related operations.*/
