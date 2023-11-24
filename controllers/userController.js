f90355ddab6c6de00dd1c409ad464bbd2bfadd01
RamboProg <smido964@gmail.com>
2023-11-24 16:20:27 +0200

    Finalized the project structure

--- controllers/userController.js
+++
@@ -1,3 +0,0 @@
-/* Use for handling specific routes or groups of routes. 
-*Each controller file might correspond to a specific resource or functionality in your application. 
-For example, you could have a userController.js file that handles user-related operations.*/
