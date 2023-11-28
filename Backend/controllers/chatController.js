a97e4d3ab4c07ca23e32daffb39d55d2ea383186
standsuser <marioma03@gmail.com>
2023-11-28 20:55:49 +0200

    cleanup

--- Backend/controllers/chatController.js
+++ Backend/controllers/chatController.js
@@ -29,20 +29,4 @@ const saveChat = async (message, fromId, toId) => { //function for saving chats
   };
   
   
-  //------------------------------
-  // import express from 'express';
-  // // controllers
-  // import chatRoom from '../controllers/chatRoom.js';
-  
-  // const router = express.Router();
-  
-  // router
-  //   .get('/', chatRoom.getRecentConversation)
-  //   .get('/:roomId', chatRoom.getConversationByRoomId)
-  //   .post('/initiate', chatRoom.initiate)
-  //   .post('/:roomId/message', chatRoom.postMessage)
-  //   .put('/:roomId/mark-read', chatRoom.markConversationReadByRoomId)
-  
-  // export default router;
-  
   
