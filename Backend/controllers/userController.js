67eeebada9d3f47ca95d8d1230c2ba62adcc928f
yahiaehab10 <102036052+yahiaehab10@users.noreply.github.com>
2023-11-28 19:43:24 +0200

    User management Finished point: 1(Register & Login) & 3(View Profile)

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -4,27 +4,18 @@ require('dotenv').config();
 const securityKey = "S12k4r4,.;lp";
 const bcrypt = require('bcryptjs');
 
-function generateSalt() {
-    //return Math.round((new Date().valueOf() * Math.random())) + '';
-    Crypto.randomBytes('256', function (err, buf) {
-        if (err) throw err;
-        return buf;
-    });
-}
-
 const userController = {
+    // Register user
     registerUser: async (req, res) => {
         try {
             const { email, password, username, phoneNumber } = req.body;
 
-            // Check if user already exists
             const userExists = await userModel.findOne({ Email: email });
             if (userExists) {
                 res.status(400).json({ message: "User already exists" });
             } else {
                 const salt = generateSalt();
                 const hash = bcrypt.hashSync(password, salt);
-                // Create a new user
                 const user = await userModel.create({
                     Email: email,
                     Password: hash,
@@ -37,9 +28,8 @@ const userController = {
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
-    }
-    ,
-
+    },
+    // Login user
     loginUser: async (req, res) => {
         try {
             const { email, password } = req.body;
@@ -47,34 +37,99 @@ const userController = {
             // Find the user by email
             const user = await userModel.findOne({ Email: email });
 
-            // Check if the user exists
             if (!user) {
                 return res.status(401).json({ message: "Invalid credentials" });
             }
 
-            //get the salt
             const salt = user.Salt;
 
-            // Hash the password
             const hash = bcrypt.hashSync(password, salt);
 
-            // Verify the password
             const isPasswordValid = bcrypt.compareSync(hash, user.Password);
 
             if (!isPasswordValid) {
                 return res.status(401).json({ message: "Invalid credentials" });
             }
 
-            // Password is valid, create a JWT token
             const token = jwt.sign({ userId: user._id }, securityKey, { expiresIn: '1h' });
 
-            // Send the token to the client
             res.status(200).json({ token });
 
         } catch (error) {
-
+            res.status(500).json({ message: error.message });
         }
-    }
+    },
+
+    // View user profile
+    viewUserProfile: async (req, res) => {
+        try {
+            const userId = req.user.userId;
+            const user = await userModel.findById(userId);
+
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            res.status(200).json({ user });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    // Update user profile
+    updateUserProfile: async (req, res) => {
+        try {
+            const userId = req.user.userId;
+            const { newEmail, newUsername, newPhoneNumber } = req.body;
+
+            const user = await userModel.findById(userId);
+
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            user.Email = newEmail || user.Email;
+            user.Username = newUsername || user.Username;
+            user.PhoneNumber = newPhoneNumber || user.PhoneNumber;
+
+            await user.save();
+
+            res.status(200).json({ message: "Profile updated successfully", user });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+    //Reset password
+    resetPassword: async (req, res) => {
+        try {
+            const userEmail = req.body.email;
+            const password = req.body.password;
+            const user = await userModel.findOne({ Email: userEmail });
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            const salt = generateSalt();
+            const hash = bcrypt.hashSync(password, salt);
+            user.Password = hash;
+            user.Salt = salt;
+
+            await user.save();
+            res.status(200).json({ message: "Password reset successfully" });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+};
+
+function generateSalt() {
+    //return Math.round((new Date().valueOf() * Math.random())) + '';
+    Crypto.randomBytes('256', function (err, buf) {
+        if (err) throw err;
+        return buf;
+    });
 }
 
 module.exports = userController;
+
+// Path: Backend/controllers/userController.js
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,17 +1,18 @@
-// userRoutes.js
 const express = require('express');
 const router = express.Router();
-const { updateUserProfile } = require('../controllers/userController');
+const { registerUser, loginUser, viewUserProfile, updateUserProfile } = require('../controllers/userController');
 const { authenticateUser } = require('../middleware/authMiddleware');
 
-// Route for updating user profile
-router.put('/api/v1/profile', authenticateUser, updateUserProfile);
+router.get('/api/v1/profile', authenticateUser, viewUserProfile); //View user profile
 
-// Route for user registration
-router.post('/api/v1/register', registerUser);
+router.put('/api/v1/profile/update', authenticateUser, updateUserProfile); //Update user profile
 
-// Route for user login
-router.post('/api/v1/auth/login', loginUser);
+router.post('/api/v1/users', registerUser); //Register user
 
+router.post('/api/v1/auth/login', loginUser); //Login user
+
+router.post('/api/v1/auth/reset-password/request', resetPassword);
 
 module.exports = router;
+
+// Path: Backend/routes/userRoutes.js
