8fcf428bb770a947db1a39e4fdd93ea8c218599e
standsuser <marioma03@gmail.com>
2023-11-30 22:42:14 +0200

    savemessage done but needs updated model

--- Backend/controllers/chatController.js
+++ Backend/controllers/chatController.js
@@ -1,36 +1,38 @@
 const Chat = require('../models/chatModel.js'); 
 
-exports.chat = async (req, res) => { 
 
-//TODO chat endpoint 
-//take 
-}
+const saveMessage = async (message, ticketid, supportAgentId) => {
+  try {
+    
+    let chat = await Chat.findOne({ Ticket_ID: ticketid, Support_AgentID: supportAgentId }); //check if the chat already exists based on ticketid and supportAgentId
 
-const saveChat = async (message, fromId, toId) => { //function for saving chats
-    try {
-
-  
-      // Create or update chat document
-      const chat = new Chat({
-        ClientID: data.clientId,
-        Support_AgentID: data.supportAgentId,
-        Messages: data.messages,
-        Start_Time: data.startTime,
-        End_Time: data.endTime,
-        Message_Count: data.messages.length,
-        Issue_Type: data.issueType,
+    if (!chat) {
+      //if the chat doesn't exist, create a new one
+      chat = new Chat({
+        Ticket_ID: ticketid,
+        Support_AgentID: supportAgentId,
+        Messages: message,
+        Start_Time: new Date(),
+        End_Time: new Date(),
+        Message_Count: 1, // set initial message count
       });
-  
-      // Save the chat to the database
-      await chat.save();
-  
-      console.log('Chat saved successfully:', chat);
-    } catch (error) {
-      console.error('Error saving chat:', error);
+    } else {
+      //if the chat already exists, update the existing document
+      chat.Messages += `\n${message}`; //append the new message to string of messages
+      chat.End_Time = new Date(); //update end time
+      chat.Message_Count += 1; // increment message count
     }
-  };
-  
-  
-  
+
+    
+    await chat.save();//save chat document to the database
+
+    console.log('Message saved successfully.');
+  } catch (error) {
+    console.error('Error saving message:', error.message);
+  }
+};
+
+module.exports = saveMessage;
+
 //const user = await getUser(req)
 // const { user_id } = await getUser(req)
