3c4c5176b5c4cffd46146a6cceccd4295e983230
RamboProg <smido964@gmail.com>
2023-12-01 17:13:34 +0200

    Fixed and updated userController

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,54 +1,44 @@
-import User from '../models/userModel.js';
+import express from 'express';
+import authenticator from 'otplib';
+import bcrypt from 'bcryptjs';
+import qrcode from 'qrcode';
+import crypto from 'crypto';
+import userModel from '../models/userModel.js';
 
-<<<<<<< HEAD
-const authenticator = require('otplib');
-const securityKey = authenticator.generateSecret();
-const bcrypt = require('bcryptjs');
-const express = require('express');
-const speakeasy = require('speakeasy');
-const qrcode = require('qrcode');
-const Crypto = require('crypto');
-function generateSalt() {
+// Function to generate salt
+async function generateSalt() {
     return new Promise((resolve, reject) => {
-      crypto.randomBytes(256, (err, buf) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(buf);
-        }
-      });
+        crypto.randomBytes(256, (err, buf) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(buf);
+            }
+        });
     });
-  }
-=======
-export const userController = {
-  getUser: async (req, res) => {
-    const User = require('./models/userModel');
-    const Admin = require('./models/adminModel');
-    const Manager = require('./models/managerModel');
-    const Agent = require('./models/agentModel');
-    const Client = require('./models/clientModel');
->>>>>>> 842a9ac77024a92a5f5c4d60f3c6653124dec6eb
+}
 
-    const userId = req.params.userId;
+const userController = {
+    // Login user
+    loginUser: async (req, res) => {
+        try {
+            const { email, password, code } = req.body;
 
-    try {
-      //assuming all users of different role types are also saved in the users table
-      const user = await User.findById({ _id: userId });
+            // Find the user by email
+            const user = await userModel.findOne({ Email: email });
 
-<<<<<<< HEAD
             if (!user) {
                 return res.status(401).json({ message: "Invalid credentials" });
             }
 
             const salt = user.Salt;
-
             const hash = bcrypt.hashSync(password, salt);
-
             const isPasswordValid = bcrypt.compareSync(hash, user.Password);
 
             if (!isPasswordValid) {
                 return res.status(401).json({ message: "Invalid credentials" });
             }
+
             if (!user.MFA_Enabled) {
                 const token = jwt.sign({ userId: user._id }, securityKey, { expiresIn: '1h' });
                 return res.status(200).json({ token });
@@ -104,6 +94,7 @@ export const userController = {
             res.status(500).json({ message: error.message });
         }
     },
+
     // Reset password
     resetPassword: async (req, res) => {
         try {
@@ -115,7 +106,7 @@ export const userController = {
                 return res.status(404).json({ message: "User not found" });
             }
 
-            const salt = await generateSalt(); // Use await to get the result from the promise
+            const salt = await generateSalt();
             const hash = bcrypt.hashSync(password, salt);
             user.Password = hash;
             user.Salt = salt;
@@ -147,7 +138,7 @@ export const userController = {
         try {
             const { id } = req.cookies;
             const { code } = req.query;
-            const user = await userModel.findById(req.user.userId); // Fetch user from the database
+            const user = await userModel.findById(req.user.userId);
             const temp_secret = user.temp_secret;
 
             const verified = authenticator.check(code, temp_secret);
@@ -165,39 +156,35 @@ export const userController = {
     }
 };
 
-// Move the generateSalt function outside the userController object
-async function generateSalt() {
-    return new Promise((resolve, reject) => {
-        crypto.randomBytes(256, (err, buf) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(buf);
-            }
-        });
-    });
+// Function to get user based on role
+async function getUser(req, res) {
+    try {
+        const User = require('./models/userModel');
+        const Admin = require('./models/adminModel');
+        const Manager = require('./models/managerModel');
+        const Agent = require('./models/agentModel');
+        const Client = require('./models/clientModel');
+        const userId = req.params.userId;
+
+        switch (user.RoleID) {
+            case 1:
+                return await Admin.findById(userId);
+            case 2:
+                return await Manager.findById(userId);
+            case 3:
+                return await Agent.findById(userId);
+            case 4:
+                return await Client.findById(userId);
+            default:
+                return null; // user is not in tables
+        }
+    } catch (error) {
+        console.error('Error could not get user', error);
+        throw error;
+    }
 }
 
-module.exports = userController;
-=======
-      switch (user.RoleID) {
-        case 1:
-          return await Admin.findById(userId);
-        case 2:
-          return await Manager.findById(userId);
-        case 3:
-          return await Agent.findById(userId);
-        case 4:
-          return await Client.findById(userId);
-        default:
-          return null; // user is not in tables
-      }
-    } catch (error) {
-      console.error('Error could not get user', error);
-      throw error;
-    }
-  }
-};
+export { userController, getUser };
 
 // async function getUser(userId) {
 //     const User = require('./models/userModel');
@@ -207,7 +194,6 @@ module.exports = userController;
 //     const Client = require('./models/clientModel');
 //     try {
 //       const user = await User.findById({ _id: userId }); //assuming all users of different role types are also saved in the users table
->>>>>>> 842a9ac77024a92a5f5c4d60f3c6653124dec6eb
 
 //       switch (user.RoleID) {
 //         case 1:
