56e68f238d65aece210595463732d15309ed3266
Fery2003 <89740632+Fery2003@users.noreply.github.com>
2023-12-01 16:49:05 +0200

    moved endpoints to proper places, implemented getUser as a placeholder

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,16 +1,23 @@
-async function getUser(userId) {
+import User from '../models/userModel.js';
+
+export const userController = {
+  getUser: async (req, res) => {
     const User = require('./models/userModel');
     const Admin = require('./models/adminModel');
     const Manager = require('./models/managerModel');
     const Agent = require('./models/agentModel');
     const Client = require('./models/clientModel');
+
+    const userId = req.params.userId;
+
     try {
-      const user = await User.findById({ _id: userId }); //assuming all users of different role types are also saved in the users table
-  
+      //assuming all users of different role types are also saved in the users table
+      const user = await User.findById({ _id: userId });
+
       switch (user.RoleID) {
         case 1:
           return await Admin.findById(userId);
-        case 2:S
+        case 2:
           return await Manager.findById(userId);
         case 3:
           return await Agent.findById(userId);
@@ -19,10 +26,37 @@ async function getUser(userId) {
         default:
           return null; // user is not in tables
       }
-  
     } catch (error) {
-      console.error('Error could not get user', error)
+      console.error('Error could not get user', error);
       throw error;
     }
   }
-  
+};
+
+// async function getUser(userId) {
+//     const User = require('./models/userModel');
+//     const Admin = require('./models/adminModel');
+//     const Manager = require('./models/managerModel');
+//     const Agent = require('./models/agentModel');
+//     const Client = require('./models/clientModel');
+//     try {
+//       const user = await User.findById({ _id: userId }); //assuming all users of different role types are also saved in the users table
+
+//       switch (user.RoleID) {
+//         case 1:
+//           return await Admin.findById(userId);
+//         case 2:
+//           return await Manager.findById(userId);
+//         case 3:
+//           return await Agent.findById(userId);
+//         case 4:
+//           return await Client.findById(userId);
+//         default:
+//           return null; // user is not in tables
+//       }
+
+//     } catch (error) {
+//       console.error('Error could not get user', error)
+//       throw error;
+//     }
+//   }
