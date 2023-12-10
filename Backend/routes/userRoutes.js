1ff133e51c26e2f52c71511012025d564fb69027
yahiaehab10 <102036052+yahiaehab10@users.noreply.github.com>
2023-12-10 17:33:33 +0200

    Fix to User route

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -8,6 +8,10 @@ const verifyJWT = require('../middleware/verifyJWT')
 
 router.post('/api/v1/auth/login', loginUser); //Login user
 
+router.get('/api/v1/profile', viewUserProfile); //View user profile
+
+router.put('//api/v1/users/:userId', updateUserProfile); //Update user profile
+
 router.post('/api/v1/auth/reset-password/request', resetPassword); //Reset password request
 
 router.get('/api/v1/auth/QrImage', getQRImage);
@@ -16,9 +20,7 @@ router.get('/api/v1/auth/setMFA', setMFA);
 
 router.use(verifyJWT)
 
-router.get('/api/v1/profile', viewUserProfile); //View user profile
 
-router.put('/api/v1/profile/update', updateUserProfile); //Update user profile
 
 
 module.exports = router;
