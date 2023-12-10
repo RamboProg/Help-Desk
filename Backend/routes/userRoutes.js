a0699ca2ec39cfd066808bd3a41330df3fe7fd29
yahiaehab10 <102036052+yahiaehab10@users.noreply.github.com>
2023-12-10 17:21:27 +0200

    Refactor adminController, userController, adminRoutes and userRoutes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { viewUserProfile, updateUserProfile, resetPassword, getQRImage, setMFA } = require('../controllers/userController');
-const {loginUser} = require('../controllers/authController');
+const { loginUser } = require('../controllers/authController');
 const { authenticateUser } = require('../middleware/authenticationMiddleware');
 const verifyJWT = require('../middleware/verifyJWT')
 
-router.post('/api/v1/users', registerUser); //Register user
 
 router.post('/api/v1/auth/login', loginUser); //Login user
 
-router.post('/api/v1/auth/reset-password/request', resetPassword);
+router.post('/api/v1/auth/reset-password/request', resetPassword); //Reset password request
 
 router.get('/api/v1/auth/QrImage', getQRImage);
 
@@ -19,7 +18,7 @@ router.use(verifyJWT)
 
 router.get('/api/v1/profile', viewUserProfile); //View user profile
 
-router.put('/api/v1/profile/update',  updateUserProfile); //Update user profile
+router.put('/api/v1/profile/update', updateUserProfile); //Update user profile
 
 
 module.exports = router;
