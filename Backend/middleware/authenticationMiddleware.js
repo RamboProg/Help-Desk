78e234181f6f3fe50c416c8dbb57ab5b5821b154
standsuser <marioma03@gmail.com>
2023-12-11 03:40:20 +0200

    authentication Middleware server errors solved

--- Backend/middleware/authenticationMiddleware.js
+++ Backend/middleware/authenticationMiddleware.js
@@ -1,27 +1,28 @@
-const jwt  = require("jsonwebtoken");
-const User = require('../models/User');
-const authenticator = require('otplib');
+const jwt = require("jsonwebtoken");
+const User = require('../models/userModel');
+const otplib = require('otplib');
+const authenticator = otplib.authenticator;
+
+// Generate a unique secret for each authentication
 const secretKey = authenticator.generateSecret();
 
-
 module.exports = async function authenticationMiddleware(req, res, next){
-     const token = req.header('Authorization');
+  const token = req.header('Authorization');
 
-     if (!token) {
-       return res.status(401).json({ message: 'Access denied. No token provided.' });
-     }
-   
-     try {
-       // Verify the token and decode 
-       const decoded = jwt.verify(token, secretKey);
+  if (!token) {
+    return res.status(401).json({ message: 'Access denied. No token provided.' });
+  }
 
-       const user = await User.findById(decoded.userId);
-        // Attach the decoded user information to the request object
-       if(!user) return res.status(401).json({ message: 'Invalid token. User not Found.' });
-          req.user = user;
-       next();
-     } catch (error) {
-       res.status(401).json({ message: 'Invalid token.' });
-     }
+  try {
+    // Verify the token and decode 
+    const decoded = jwt.verify(token, secretKey);
+
+    const user = await User.findById(decoded.userId);
+    // Attach the decoded user information to the request object
+    if(!user) return res.status(401).json({ message: 'Invalid token. User not Found.' });
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token.' });
+  }
 };
-
