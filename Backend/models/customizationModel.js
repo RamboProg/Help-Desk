b9d58ee11ad26db394a98516031d45ca56ff2be2
RamboProg <smido964@gmail.com>
2023-12-12 22:22:27 +0200

    Initialized Tailwind and updated Customization

--- Backend/models/customizationModel.js
+++ Backend/models/customizationModel.js
@@ -1,1 +1,10 @@
-//add stuff here
+// customizationModel.js
+const mongoose = require('mongoose');
+
+const customizationSchema = new mongoose.Schema({
+  userId: { type: Number, required: true, unique: true },
+  theme: { type: String, required: true },
+  logoPath: { type: String },
+});
+
+module.exports = mongoose.model('Customization', customizationSchema);
