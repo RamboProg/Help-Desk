6f0d8c7774e4e0f8bf2834d6edc80017f6b46c2a
Zaid Rass <zaidqarxoy@gmail.com>
2023-12-14 20:38:17 +0200

    Assign Role Done

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -13,7 +13,64 @@ const adminController = {
     AssignRole: async (req, res) => {
         try 
         {
-        
+            const {userID, roleID} = req.body;
+            const user = await userModel.findOne({ _id: userID });
+            if (!user) {
+                return res.status(401).json({ message: "User not Found"});
+            }
+            // To delete a non-admin user
+            let oldRole;
+            switch (user.RoleID)
+            {
+                case 2: 
+                    oldRole = await managerModel.findOne({ _id: userID });
+                    break;
+                case 3:
+                    oldRole = await agentModel.findOne({ _id: userID });
+                    break;
+                case 4:
+                    oldRole = await clientModel.findOne({ _id: userID });
+                    break;
+                default: 
+                    return res.status(401).json({ message: "Invalid Role"});
+            }
+            if (!oldRole) {
+                return res.status(401).json({ message: "User not Found"});
+            }
+            await oldRole.deleteOne();
+            // To assign a new role
+            let newRole;
+            switch(roleID)
+            {
+                case 2: 
+                    newRole = await managerModel;
+                    break;
+                case 3:
+                    newRole = await agentModel;
+                    break;
+                case 4:
+                    newRole = await clientModel;
+                    break;
+                default: 
+                    return res.status(401).json({ message: "Invalid Role"});
+            }
+            if (!newRole) {
+                return res.status(401).json({ message: "User not Found"});
+            }
+            const newUser =await newRole.create({
+                _id: userID,
+                Email: user.Email,
+                Password: user.Password,
+                Username: user.Username,
+                PhoneNumber: user.PhoneNumber,
+                RoleID: roleID,
+                MFA_Enabled: user.MFA_Enabled,
+                Is_EnabledL: user.Is_Enabled,
+                Salt: user.Salt,
+             });
+             await userModel.updateOne({_id: userID}, {RoleID: roleID});
+             return res.status(200).json({message: "Role Assigned Successfully"});
+
         }catch(error){return res.status(401).json({message:error.message})}
         
        
