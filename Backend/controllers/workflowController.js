5b6d068b363d0158c207731b1319a42f8c167510
standsuser <marioma03@gmail.com>
2023-12-23 00:58:06 +0200

    delete workflow created and update workflow deleted

--- Backend/controllers/workflowController.js
+++ Backend/controllers/workflowController.js
@@ -5,11 +5,11 @@ exports.getWorkflow = async (req, res) => {
   try {
     // Use findById to fetch the document by its _id
     const workflow = await Workflow.findById(Issue); // Assuming Issue is the _id value
-  
+
     if (!workflow) {
       return res.status(404).json({ message: 'Solutions not found' });
     }
-  
+
     const customWorkflow = workflow.Custom_Workflow;
     res.json({ Custom_Workflow: customWorkflow });
   } catch (err) {
@@ -18,23 +18,27 @@ exports.getWorkflow = async (req, res) => {
 };
 
 exports.createWorkflow = async (req, res) => {
-    const { Issue, Custom_Workflow , Sub_Issue_Type} = req.body;
-    try {
-      const workflow = await Workflow.create({ Issue, Custom_Workflow });
-      res.status(201).json({ workflow });
-    } catch (err) {
-      console.error("Error creating workflow:", err); // Log the error details for debugging
-      res.status(500).json({ message: "Internal Server Error; Try Again" });
-    }
-    
-}
+  const { Issue, Custom_Workflow, Sub_Issue_Type } = req.body;
+  try {
+    const workflow = await Workflow.create({ Issue, Custom_Workflow, Sub_Issue_Type });
+    res.status(201).json({ workflow });
+  } catch (err) {
+    console.error("Error creating workflow:", err); // Log the error details for debugging
+    res.status(500).json({ message: "Internal Server Error; Try Again" });
+  }
 
-exports.updateWorkflow = async (req, res) => {
-    const { Issue, Custom_Workflow } = req.body;
-    try {
-      const workflow = await Workflow.findOneAndUpdate({ Issue }, { Custom_Workflow });
-      res.status(201).json({ workflow });
-    } catch (err) {
-      res.status(500).json({ message: "Internal Server Error; Try Again" });
-    }
 }
+exports.deleteWorkflow = async (req, res) => {
+  const { issuesid } = req.query; // Destructure the issuesid from req.params
+  try {
+    const deleted = await Workflow.deleteOne({ _id: issuesid }); // Use _id to match the ObjectId
+    if (deleted.deletedCount === 0) {
+      return res.status(404).json({ message: 'Workflow not found' });
+    }
+    res.status(200).json({ message: 'Workflow deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error; Try Again" });
+  }
+};
+
+
--- Backend/routes/workflowRoute.js
+++ Backend/routes/workflowRoute.js
@@ -6,6 +6,6 @@ const workflowController = require('../controllers/workflowController');
 // Get Custom_Workflow based on Issue
 router.get('/api/v1/workflows/:issuesid', workflowController.getWorkflow);
 router.put('/api/v1/workflows', workflowController.createWorkflow);
-router.post('/api/v1/workflows/:issuesid', workflowController.updateWorkflow);
+router.delete('/api/v1/workflows/:issuesid', workflowController.deleteWorkflow);
 
 module.exports = router;
