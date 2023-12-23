e5a34316ea0aef36563fec77a9184df5c892368a
Zaid Rass <zaidqarxoy@gmail.com>
2023-12-23 15:32:16 +0200

    Assign role Done

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,5 @@
 // App.js
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainLayout from './MainLayout';
@@ -11,42 +12,29 @@ import ManagerHome from './components/ManagerHome';
 import AgentHome from './components/AgentHome';
 import KnowledgeBase from './components/KnowledgeBase';
 import AssignRole from './components/AssignRole';
+import AdminNav from './components/AdminNav';
+
+function AdminLayout({ children }) {
+  return (
+    <MainLayout>
+      <AdminNav />
+      {children}
+    </MainLayout>
+  );
+}
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={<MainLayout><Homepage /></MainLayout>}
-        />
-        <Route
-          path="/login"
-          element={<MainLayout><Login /></MainLayout>}
-        />
-        <Route
-          path="/ClientHome"
-          element={<ClientLayout><ClientHome /></ClientLayout>}
-        />
-        <Route
-          path="/AdminHome"
-          element={<MainLayout><AdminHome /></MainLayout>}
-        />
-        <Route
-          path="/ManagerHome"
-          element={<MainLayout><ManagerHome /></MainLayout>}
-        />
-        <Route
-          path="/AgentHome"
-          element={<MainLayout><AgentHome /></MainLayout>}
-        />
-        <Route
-          path="/KnowledgeBase"
-          element={<ClientLayout><KnowledgeBase /></ClientLayout>}
-        />
-        <Route 
-          path="/AssignRole"
-          element={<MainLayout><AssignRole /></MainLayout>}/>
+        <Route path="/" element={<MainLayout><Homepage /></MainLayout>} />
+        <Route path="/login" element={<MainLayout><Login /></MainLayout>} />
+        <Route path="/ClientHome" element={<ClientLayout><ClientHome /></ClientLayout>} />
+        <Route path="/AdminHome" element={<AdminLayout><AdminHome /></AdminLayout>} />
+        <Route path="/ManagerHome" element={<MainLayout><ManagerHome /></MainLayout>} />
+        <Route path="/AgentHome" element={<MainLayout><AgentHome /></MainLayout>} />
+        <Route path="/KnowledgeBase" element={<ClientLayout><KnowledgeBase /></ClientLayout>} />
+        <Route path="/AssignRole" element={<AdminLayout><AssignRole /></AdminLayout>} />
       </Routes>
     </Router>
   );
--- Frontend/src/components/RoleUpdatePopup.jsx
+++ Frontend/src/components/RoleUpdatePopup.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-// RoleUpdatePopup component with inline CSS styles
+// RoleUpdatePopup component with improved styles
 const RoleUpdatePopup = ({ newRoleId, onUpdate, onClose, onRoleChange, style }) => {
   const popupStyles = {
     position: "fixed",
@@ -20,22 +20,61 @@ const RoleUpdatePopup = ({ newRoleId, onUpdate, onClose, onRoleChange, style }) 
     borderRadius: "8px",
     width: "300px", // Set the width as needed
     textAlign: "center",
+    boxShadow: "0 2px 10px rgba(0, 0, 0, 0.2)", // Add a subtle box shadow
     ...style, // Merge external styles
+  };
+
+  const labelStyles = {
+    display: "block",
+    marginBottom: "10px",
+    color: "#333",
+  };
+
+  const selectStyles = {
+    width: "100%",
+    padding: "8px",
+    marginBottom: "15px",
+    boxSizing: "border-box",
+  };
+
+  const buttonStyles = {
+    backgroundColor: "#4caf50",
+    color: "#fff",
+    padding: "10px",
+    border: "none",
+    borderRadius: "4px",
+    marginRight: "10px",
+    cursor: "pointer",
+  };
+
+  const cancelButtonStyles = {
+    backgroundColor: "#ddd",
+    color: "#333",
+    padding: "10px",
+    border: "none",
+    borderRadius: "4px",
+    cursor: "pointer",
   };
 
   return (
     <div style={{ ...popupStyles, ...style }}>
       <div style={popupInnerStyles}>
-        <h2>Update User Role</h2>
-        <label htmlFor="roleSelect">Select Role:</label>
-        <select id="roleSelect" value={newRoleId} onChange={(e) => onUpdate(e.target.value)}>
+        <h2 style={{ color: "#2196F3" }}>Update User Role</h2>
+        <label htmlFor="roleSelect" style={labelStyles}>
+          Select Role:
+        </label>
+        <select id="roleSelect" value={newRoleId} onChange={(e) => onUpdate(e.target.value)} style={selectStyles}>
           <option value="2">Manager</option>
           <option value="3">Support Agent</option>
           <option value="4">Client</option>
         </select>
         <br />
-        <button onClick={onRoleChange}>Update Role</button>
-        <button onClick={onClose}>Cancel</button>
+        <button onClick={onRoleChange} style={buttonStyles}>
+          Update Role
+        </button>
+        <button onClick={onClose} style={cancelButtonStyles}>
+          Cancel
+        </button>
       </div>
     </div>
   );
