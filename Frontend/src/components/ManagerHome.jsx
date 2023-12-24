ed9dc2ab0ecad20482bd206986d5985a282083e7
standsuser <marioma03@gmail.com>
2023-12-24 15:33:18 +0200

    changed home bg images

--- Frontend/src/components/ManagerHome.jsx
+++ Frontend/src/components/ManagerHome.jsx
@@ -29,7 +29,7 @@ const Manager = () => {
     <div 
       className="bg-cover bg-center h-screen flex justify-center items-center" 
       style={{
-        backgroundImage: "url('https://images.unsplash.com/photo-1531482615713-2afd69097998?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
+        backgroundImage: "url('https://images.unsplash.com/photo-1557426272-fc759fdf7a8d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
       }}
     >
       <div className='max-w-[1640px] mx-auto px-4 py-12 text-center bg-white bg-opacity-50 rounded-lg p-8'>
