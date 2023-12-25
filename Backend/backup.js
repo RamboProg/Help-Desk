548732598a9711079ff00fe51d084ff0d6256e2a
Zaid Rass <zaidqarxoy@gmail.com>
2023-12-25 23:23:23 +0200

    BACK UP AND RESTORE WORKS

--- Backend/backup.js
+++ Backend/backup.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cron = require('node-cron');
 const dotenv = require('dotenv');
 const path = require('path');
+const { spawn } = require('child_process');
 const { exec } = require('child_process');
 
 dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 
-const port = 3001;
 const dbUrl = process.env.MONGODB_URI;
 console.log(dbUrl);
 const backupPath = process.env.BACKUP_DIR;
@@ -16,7 +16,7 @@ const backupPath = process.env.BACKUP_DIR;
 const performBackup = () => {
   const timestamp = new Date().toISOString().replace(/:/g, '-');
 
-  const command = `mongodump --uri="${dbUrl}"`;
+  const command = `mongodump --uri="${dbUrl}" --gzip`;
 
   exec(command, (error, stdout, stderr) => {
     if (error) {
@@ -28,10 +28,44 @@ const performBackup = () => {
 };
 
 // Schedule the backup to run every day at midnight
-cron.schedule('0 0 * * *', () => {
-  performBackup();
-});
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const performRestore = () => {
+  const mongoUriRestore = process.env.MONGODB_URI_RESTORE;
+
+  console.log("Restoring backup");
+  const backUpPath = "C:\\Users\\Elnour Tech\\OneDrive\\Desktop\\Software Project\\Help-Desk\\Backend"; // Adjust this path
+  console.log(backUpPath);
+  console.log(mongoUriRestore);
+  const mongorestoreCommand = "mongorestore";
+  const argsRestore = [
+    `--uri=${mongoUriRestore}`,
+    `--gzip`, // Optional: if the backup was created with mongodump --gzip // Optional: drop collections before restoring
+    `${backUpPath}\\dump`
+  ];
+
+  const mongorestoreProcess = spawn(mongorestoreCommand, argsRestore);
+
+  mongorestoreProcess.stdout.on("data", (data) => {
+    console.log(`mongorestore stdout: ${data}`);
+  });
+
+  mongorestoreProcess.stderr.on("data", (data) => {
+    console.error(`mongorestore stderr: ${data}`);
+  });
+
+  mongorestoreProcess.on("close", (code) => {
+    console.log(`mongorestore process exited with code ${code}`);
+  });
+
+  mongorestoreProcess.on("error", (err) => {
+    console.error("Failed to start mongorestore process:", err);
+  });
+};
+
+ cron.schedule('0 0 * * * *', () => {
+  performBackup();
+  performRestore();
+ });
+
+
+
