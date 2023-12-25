1a68420dfd0aed0fd2653dd08ad14035173ebfed
Aboelwa7awe7 <mohamedelwa7a@gmail.com>
2023-12-26 01:57:21 +0200

    various fixes

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -6,25 +6,4 @@ const router = express.Router();
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
 
-module.exports = async function(req, res, next) {
-  const sessionToken = getSessionToken(req);
-  if (!sessionToken) {
-    return res.status(301).redirect('/');
-  }
-
-  // We then get the session of the user from our session map
-  // that we set in the signinHandler
-  const userSession = await db.select('*').from('se_project.sessions').where('token', sessionToken).first();
-  if (!userSession) {
-    // If the session token is not present in session map, return an unauthorized error
-    return res.status(301).redirect('/');
-  }
-  // if the session has expired, return an unauthorized error, and delete the 
-  // session from our map
-  if (new Date() > userSession.expiresat) {
-    return res.status(301).redirect('/');
-  }
-
-  // If all checks have passed, we can consider the user authenticated and
-  next();
-};
+module.exports = router;
