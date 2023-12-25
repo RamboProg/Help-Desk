67f0acb4e2765d9fd011a3364f48d8d032efa5df
yahiaehab10 <102036052+yahiaehab10@users.noreply.github.com>
2023-12-25 16:24:37 +0200

    Refactor userRoutes and Profile component

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { userController } = require('../controllers/userController');
-const authorizationMiddleware= require('../middleware/authorizationMiddleware');
+const authorizationMiddleware = require('../middleware/authorizationMiddleware');
 
 // Apply the authentication middleware to protected routes
 // 1: Admin, 2: Manager, 3: Support Agent, 4: Client
-router.get('/profile', authorizationMiddleware([1,2,3,4]), userController.viewUserProfile);
-// router.put('/api/v1/users/:userId', authorizationMiddleware([1,2,3,4]), userController.updateUserProfile);
+router.get('/profile', authorizationMiddleware([1, 2, 3, 4]), userController.viewUserProfile);
+router.put('/api/v1/users/:userId', authorizationMiddleware([1, 2, 3, 4]), userController.updateUserProfile);
 
 
 // Other routes...
--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -1,29 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineTool,
+  AiOutlineProfile,
+} from "react-icons/ai";
 
 const Profile = () => {
   const [user, setUser] = useState(null);
-  const [newEmail, setNewEmail] = useState('');
-  const [newUsername, setNewUsername] = useState('');
-  const [newPhoneNumber, setNewPhoneNumber] = useState('');
+  const [newEmail, setNewEmail] = useState("");
+  const [newUsername, setNewUsername] = useState("");
+  const [newPhoneNumber, setNewPhoneNumber] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Fetch user profile data
     const fetchUserProfile = async () => {
       try {
         const response = await axios.get(
-          'http://localhost:3000/api/v1/users/profile',
-
+          "http://localhost:3000/api/v1/users/profile",
           { withCredentials: true }
         );
-        console.log('Profile Response:', response);
 
         setUser(response.data.user);
         setNewEmail(response.data.user.Email);
         setNewUsername(response.data.user.Username);
         setNewPhoneNumber(response.data.user.PhoneNumber);
       } catch (error) {
-        console.error('Error fetching user profile:', error.message);
+        console.error("Error fetching user profile:", error.message);
       }
     };
 
@@ -32,17 +38,21 @@ const Profile = () => {
 
   const handleUpdateProfile = async () => {
     try {
-      const response = await axios.put(`http://localhost:3000/api/v1/users/${user._id}`, {
-        newEmail,
-        newUsername,
-        newPhoneNumber
-      });
+      const response = await axios.put(
+        `http://localhost:3000/api/v1/users/${user._id}`,
+        {
+          newEmail,
+          newUsername,
+          newPhoneNumber,
+        }
+      );
 
       console.log(response.data.message);
-      // Optionally, you can update the user state with the updated data if needed
       setUser(response.data.user);
+
+      navigate("/AdminHome");
     } catch (error) {
-      console.error('Profile update failed:', error.message);
+      console.error("Profile update failed:", error.message);
     }
   };
 
@@ -51,23 +61,79 @@ const Profile = () => {
   }
 
   return (
-    <div>
-      <h1>User Profile</h1>
-      <form onSubmit={handleUpdateProfile}>
-        <div>
-          <label>Email:</label>
-          <input type='text' value={newEmail} onChange={(e) => setNewEmail(e.target.value)} />
-        </div>
-        <div>
-          <label>Username:</label>
-          <input type='text' value={newUsername} onChange={(e) => setNewUsername(e.target.value)} />
-        </div>
-        <div>
-          <label>Phone Number:</label>
-          <input type='text' value={newPhoneNumber} onChange={(e) => setNewPhoneNumber(e.target.value)} />
-        </div>
-        <button type='submit'>Update Profile</button>
-      </form>
+    <div className="flex">
+      <div className="bg-gray-800 text-white h-screen w-1/6 p-5">
+        <ul className="space-y-4">
+          <li className="flex items-center">
+            <AiOutlineHome className="mr-2" />
+            <button
+              onClick={() => navigate("/AdminHome")}
+              className="hover:underline focus:outline-none">
+              Home
+            </button>
+          </li>
+          <li className="flex items-center">
+            <AiOutlineTool className="mr-2" />
+            <button
+              onClick={() => navigate("/AssignRole")}
+              className="hover:underline focus:outline-none">
+              AssignRole
+            </button>
+          </li>
+          <li className="flex items-center">
+            <AiOutlineTool className="mr-2" />
+            <button
+              onClick={() => navigate("/Settings")}
+              className="hover:underline focus:outline-none">
+              Settings
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className="max-w-[1640px] m-auto px-4 py-12 flex-grow ">
+        <form
+          onSubmit={handleUpdateProfile}
+          className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-md">
+          <div className="mb-4">
+            <label className="block text-sm font-semibold text-gray-600">
+              Email:
+            </label>
+            <input
+              type="text"
+              value={newEmail}
+              onChange={(e) => setNewEmail(e.target.value)}
+              className="w-full p-2 border rounded-md"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold text-gray-600">
+              Username:
+            </label>
+            <input
+              type="text"
+              value={newUsername}
+              onChange={(e) => setNewUsername(e.target.value)}
+              className="w-full p-2 border rounded-md"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold text-gray-600">
+              Phone Number:
+            </label>
+            <input
+              type="text"
+              value={newPhoneNumber}
+              onChange={(e) => setNewPhoneNumber(e.target.value)}
+              className="w-full p-2 border rounded-md"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded-full">
+            Update Profile
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
