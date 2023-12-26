826293945c9924034843997367be1adc7a5e12af
standsuser <marioma03@gmail.com>
2023-12-26 19:13:01 +0200

    chat redirection

--- Frontend/src/components/AgentNav.jsx
+++ Frontend/src/components/AgentNav.jsx
@@ -75,7 +75,7 @@ const AgentNav = () => {
               {/* Chats */}
               <li
                 className="text-xl py-4 flex items-center transition ease-in-out duration-300 hover:bg-blue-50 hover:shadow-md cursor-pointer"
-                onClick={() => navigate('/ViewChats')}
+                onClick={() => navigate('/Chat')}
               >
                 <AiOutlineMessage size={20} className="mr-2" />
                 Chats
